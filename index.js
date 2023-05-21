8abbc5cf854155426332cd70cc65705bd1dead6c
Ricardo Piedade <98702051+Acr2004@users.noreply.github.com>
2023-05-21 01:04:41 +0100

    Version 1.0.2

--- index.js
+++ index.js
@@ -16,3 +16,32 @@ const typed = new Typed('.job', {
     backDelay: 1000,
     loop: true
 });
+
+window.addEventListener("scroll", () => {
+    const sections = document.querySelectorAll("section");
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        let sectionHeight = current.offsetHeight;
+        let sectionTop = current.offsetTop - 88;
+        let id = current.getAttribute("id");
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector("#nav-bar-list a[href*=" + id + "]").classList.add("active");
+        }
+        else {
+            document.querySelector("#nav-bar-list a[href*=" + id + "]").classList.remove("active");
+        }
+    });
+})
+
+const scrollToTop = document.querySelector(".scrollToTop");
+
+window.addEventListener("scroll", function() {
+    scrollToTop.classList.toggle("show", window.scrollY > 500);
+})
+
+scrollToTop.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
